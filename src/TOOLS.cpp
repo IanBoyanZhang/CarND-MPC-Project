a49f4fcd5e2ebc7170fea791fa3415b3bb4f4321
Ian Zhang <ianzhang1986@gmail.com>
2017-06-08 20:14:15 -0700

    [Feature] Remove redundant functions

--- src/TOOLS.cpp
+++ src/TOOLS.cpp
@@ -10,28 +10,6 @@ double Tools::mph_to_mps(double v) {
   return v * 1609/3600;
 }
 
-// Fit a polynomial.
-// Adapted from
-// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
-/*VectorXd Tools::polyfit(VectorXd xvals, VectorXd yvals, int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
-  Eigen::MatrixXd A(xvals.size(), order + 1);
-
-  for (int i = 0; i < xvals.size(); i++) {
-    A(i, 0) = 1.0;
-  }
-
-  for (int j = 0; j < xvals.size(); j++) {
-    for (int i = 0; i < order; i++) {
-      A(j, i + 1) = A(j, i) * xvals(j);
-    }
-  }
-
-  auto Q = A.householderQr();
-  return Q.solve(yvals);
-}*/
-
 /**
  * Fit a polynomial
  * Adapted from
